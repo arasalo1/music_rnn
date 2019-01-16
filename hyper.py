46c424f025517245d5e55518fe3b2ae6a31496a6
Ossi Arasalo <ossi.arasalo@aalto.fi>
2019-01-16 13:25:10 +0200

    hyper

--- hyper.py
+++ hyper.py
@@ -3,70 +3,54 @@ import pypianoroll as pp
 import tensorflow as tf
 import keras
 from Generator_compressed import Generator_compressed
-from Parser import Parser
-import matplotlib as mpl 
-mpl.use('Agg')
-import matplotlib.pyplot as plt
 
-sl = 100
-b = 32
-print("load training")
-training_generator = Generator_compressed('../lpd',file_name="train_names.txt",sequence_length=sl,batch_size=b)
-print("load validation")
-validation_generator = Generator_compressed('../lpd_valid',file_name="test_names.txt",sequence_length=sl,batch_size=b)
+from hyperopt import Trials, STATUS_OK, tpe
+import hyperas
+from hyperas.distributions import choice, uniform
 
-model = keras.Sequential()
-model.add(keras.layers.CuDNNLSTM(128,input_shape=(sl,128),return_sequences=True))
-model.add(keras.layers.Dropout(0.3))
-model.add(keras.layers.CuDNNLSTM(512,return_sequences=True))
-model.add(keras.layers.Dropout(0.3))
-model.add(keras.layers.CuDNNLSTM(256))
-model.add(keras.layers.Dense(256,activation='elu'))
-model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(128,activation='elu'))
-model.add(keras.layers.Dense(128,activation='softmax'))
+def data():
+    sl = 100
+    b = 16
+    subset=0.1
+    print("load training")
+    training_generator = Generator_compressed('../lpd',file_name="train_names.txt",sequence_length=sl,batch_size=b, subset=subset)
+    print("load validation")
+    validation_generator = Generator_compressed('../lpd_valid',file_name="test_names.txt",sequence_length=sl,batch_size=b, subset=subset)
+    
+    return training_generator, validation_generator
 
-model.compile(loss='categorical_crossentropy',optimizer='adam')
+def model(datagen):
 
-history = model.fit_generator(generator=training_generator,
-					validation_data=validation_generator,
-                    use_multiprocessing=True,
-                    epochs=15,
-                    workers=10)
+    model = keras.Sequential()
+    model.add(keras.layers.CuDNNLSTM(128,input_shape=(sl,128),return_sequences=True))
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.CuDNNLSTM(512,return_sequences=True))
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.CuDNNLSTM(256))
+    model.add(keras.layers.Dense(256,activation='elu'))
+    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.Dense(128,activation='elu'))
+    model.add(keras.layers.Dense(128,activation='softmax'))
 
-model.save("compressed7.h5")
+    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-fig,ax = plt.subplots()
-ax.plot(history.history['loss'])
-ax.plot(history.history['val_loss'])
-ax.set_title('Loss')
-ax.set_ylabel('loss')
-ax.set_xlabel('epoch')
-ax.legend(['train', 'test'], loc='upper right')
-fig.savefig("loss2.png")
+    history = model.fit_generator(generator=datagen[0],
+                        validation_data=datagen[1],
+                        use_multiprocessing=True,
+                        epochs=4,
+                        workers=10)
+    
+    validation_acc = np.amax(result.history['val_acc']) 
 
-print("Create a sample")
-files = validation_generator.__getitem__(21)
-init = files[0][4:5]
-le = 300
-clip = np.empty([le,128])
-for i in range(le):
-    print("Predicting: %i"%i,end='\r')
-    pred = model.predict(init)[0,]
-    prediction = np.zeros(128)
-    prediction[np.argmax(pred)] = 1
-    #prediction[0] = 0
-    prediction[127] = 0
-    clip[i,] = prediction
-    init = np.roll(init,-1,axis=1)
-    init[0,-1,] = prediction
+    return {'loss': -validation_acc, 'status': STATUS_OK, 'model': model}
 
-print("\nsaving")
-np.save('out2.npy',clip)
+best_run, best_model = optim.minimize(model=model,data=data,
+                                        algo=tpe.suggest,
+                                        max_evals=5,
+                                        trials=Trials())
+
+print("Evalutation of best performing model:")
+#print(best_model.evaluate(X_test, Y_test))
+print(best_run)
+
 keras.backend.clear_session()
-#midi_file = 'out2.mid'
-#pp.Multitrack(tracks=[pp.Track(pianoroll=clip)]).write(midi_file)
-
-#fig,ax = pp.Track(pianoroll=clip).plot()
-#fig.savefig("song.png")
-#print("finished")
